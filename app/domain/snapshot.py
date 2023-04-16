2c3f4cc78075e9eefd063e32af4e6d7c0975a8ba
pctmoraes <paula.ct.moraes@gmail.com>
2023-04-16 09:53:31 -0300

    Adds type validation to the class attribute

--- app/domain/snapshot.py
+++ app/domain/snapshot.py
@@ -8,10 +8,18 @@ class Snapshot:
         resource (Resource): A Resource object.
     """
     def __init__(self, resource:Resource) -> None:
+        if type(resource) is not Resource:
+            raise TypeError("The resource name must be a Resource object.")
         self.__resource = resource
         self.__creation_date = datetime.now()
     
     def get_resource(self) -> Resource:
+        """
+        Gets the snapshot resource.
+
+        Returns:
+            A Resource object
+        """
         return self.__resource
     
     def get_creation_date(self) -> datetime:
