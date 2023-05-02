3a11232a27f861811fb60eba28a7873ede18d02f
pctmoraes <paula.ct.moraes@gmail.com>
2023-05-02 09:13:05 -0300

    Code refactor on retention_plan_service

--- app/service/retention_plan_service.py
+++ app/service/retention_plan_service.py
@@ -14,34 +14,31 @@ def should_retain_snapshot(retention_plan:RetentionPlan) -> bool:
     retention_plan_date = retention_plan.get_retention_date()
     retention_period = timedelta(days=42)
     
-    if (snapshot_date + retention_period) > retention_plan_date:
-        return False
+    if retention_plan.get_retention_type() == "STANDARD":
+        return retention_plan_date >= datetime.now()
+
     else:
-        if retention_plan.get_retention_type() == "STANDARD":
+        next_snapshot_month = snapshot_date + timedelta(days=28)
+        last_day_of_month = (next_snapshot_month.replace(day=28) + timedelta(days=4)) \
+                            .replace(day=1) - timedelta(days=1)
+        last_snapshot_of_month = last_day_of_month - timedelta(days=last_day_of_month.day)
+        retention_period_monthly = timedelta(days=365)
+        
+        if retention_plan.get_retention_type() == "GOLD":
+            if snapshot_date == last_snapshot_of_month:
+                retention_period += retention_period_monthly
+            
             expiration_date = snapshot_date + retention_period
             return expiration_date >= datetime.now()
-
+        
         else:
-            last_day_of_month = (snapshot_date.replace(day=28) + timedelta(days=4)) \
-                                    .replace(day=1) - timedelta(days=1)
-            last_snapshot_of_month = last_day_of_month - timedelta(days=last_day_of_month.day)
-            retention_period_monthly = timedelta(days=365)
+            last_snapshot_of_year = datetime(snapshot_date.year, 12, 31)
+            retention_period_yearly = timedelta(days= 7 * 365)
             
-            if retention_plan.get_retention_type() == "GOLD":
-                if snapshot_date == last_snapshot_of_month:
-                    retention_period += retention_period_monthly
-                
-                expiration_date = snapshot_date + retention_period
-                return expiration_date >= datetime.now()
-            
-            else:
-                last_snapshot_of_year = datetime(snapshot_date.year, 12, 31)
-                retention_period_yearly = timedelta(days= 7 * 365)
-                
-                if snapshot_date == last_snapshot_of_month:
-                    retention_period += retention_period_monthly
-                if snapshot_date == last_snapshot_of_year:
-                    retention_period += retention_period_yearly
+            if snapshot_date == last_snapshot_of_month:
+                retention_period += retention_period_monthly
+            if snapshot_date == last_snapshot_of_year:
+                retention_period += retention_period_yearly
 
-                expiration_date = snapshot_date + retention_period
-                return expiration_date >= datetime.now()
+            expiration_date = snapshot_date + retention_period
+            return expiration_date >= datetime.now()
